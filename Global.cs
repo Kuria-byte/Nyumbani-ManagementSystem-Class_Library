0c8468ed8f30557369157cc3f1ef85dee2ee935b
Kuria-byte <mwitumi21@gmail.com>
2021-02-04 04:24:10 +0800

    Add project files.

---
+++ Global.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library_EstateManagementSystem.Models
+{
+   public class Global
+    {
+        public static string connString = "";
+        public static Library_EstateManagementSystem.Models.mAdminLogin gAdminLogin = new Library_EstateManagementSystem.Models.mAdminLogin();
+    }
+}
---
+++ Models/mAdminLogin.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library_EstateManagementSystem.Models
+{
+    public class mAdminLogin
+    {
+        public int AdminLoginID;
+        public string AdminLoginName;
+        public string AdminLoginEmail;
+        public string AdminLoginPassword;
+        public string AdminPicture;
+        public DateTime LastLoginDateTime;
+        public bool IsActive;
+    }
+}
